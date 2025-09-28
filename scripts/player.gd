6e687b107f2873f0cd83c727506d09a591a07c95
ao-u <sam.ffish@gmail.com>
2025-09-28 03:43:48 -0400

    fsm setup

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,21 @@
+class_name player
 extends CharacterBody3D
 
-@onready var mainCamera : Camera3D = %MainCamera
+@onready var maincamera : Camera3D = %maincamera
 @onready var camerapivot : Node3D = %camerapivot
+@onready var canvas = %canvas
+@onready var uiviewport : SubViewport = %ui
+@onready var uitexture : TextureRect = %texture
+
+@onready var lefthand : Sprite3D = %lefthand
+@onready var righthand : Sprite3D = %righthand
 
 var sens : Vector2 = Vector2(1.0, 1.0)
 
-enum States {NORMAL}
-@onready var state := States.NORMAL
 
 func _ready() -> void:
 	wall_min_slide_angle = .01
+	
 	
 	
 
@@ -21,34 +27,41 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		camerapivot.rotate_y(-event.relative.x * .005 * sens.y)
-		mainCamera.rotate_x(-event.relative.y * .005 * sens.x)
-		mainCamera.rotation.x = clamp(mainCamera.rotation.x, -1.5, 1.5)
+		maincamera.rotate_x(-event.relative.y * .005 * sens.x)
+		maincamera.rotation.x = clamp(maincamera.rotation.x, -1.5, 1.5)
 		
 
 
-var maxspeed := 6.0
-var accel := 3.0
+var maxspeed := 12.0
+var accel := 4.0
+var slowaccelrate = 5
 
 var gravity := 2.0
 var jumpforce := 30.0
 
-
 var jumptimer := -1.0
 
 func _physics_process(delta: float) -> void:
+	
+	
 	var v := velocity
 	
 	var inputdir := Input.get_vector("left", "right", "up", "down");
+	inputdir = inputdir.normalized()
+	inputdir.x *= 1.1
+	
+	do_camera_tilt(inputdir)
+	
 	var wishdir := (camerapivot.transform.basis * Vector3(inputdir.x, 0, inputdir.y)).normalized()
 	
 	
-	v += wishdir * accel * clampf(maxspeed / v.length(), 0.0, 2.5)
+	v += wishdir * accel * clampf(maxspeed / v.length(), 0.0, slowaccelrate)
 	
-	var friction := .98
+	var friction := .99
 	if is_on_floor() or is_on_wall():
 		friction *= .95
 	if wishdir.length() > 0.1:
-		friction *= 1.0
+		friction *= .98
 	else:
 		friction *= .95
 	friction = min(friction, 1.0)
@@ -65,13 +78,40 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 	
-func assign_state() -> void:
-	if state == States.NORMAL:
-		print()
+	
+	pstate.PhysUpdate(self)
+
+func do_camera_tilt(inputdir : Vector2) -> void:
+	
+	camerapivot.rotation.z = lerpf(camerapivot.rotation.z, -1.0 * inputdir.x * .05, .2)
 	
 	
+		
+@onready var pstate : PlayerState = Neutral.new();
 	
 	
+@abstract class PlayerState:
+	@abstract func PhysUpdate(p : player) -> void
+	
+class Neutral extends PlayerState:
+	func PhysUpdate(p : player) -> void:
+		print("neutral")
+		if p.velocity.y > 0.0:
+			p.pstate = Ascending.new()
+		elif p.velocity.y < 0.0:
+			p.pstate = Falling.new()
+		
+class Ascending extends PlayerState:
+	func PhysUpdate(p : player) -> void:
+		print("asc")
+		if p.velocity.y <= 0.0:
+			p.pstate = Neutral.new()
+		
+class Falling extends PlayerState:
+	func PhysUpdate(p : player) -> void:
+		print("fall")
+		if p.velocity.y >= 0.0:
+			p.pstate = Neutral.new()
 	
 	
 	
